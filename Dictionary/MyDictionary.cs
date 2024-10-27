6c5a9ebbd64593a2f2e1f507e4e7d4e9a20c8722
Loneliness <Loneliness@HUMAN>
2024-10-27 12:22:42 +0500

    Add project files.

---
+++ Dictionary/MyDictionary.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Library
+{
+    private Dictionary<string, string> books = new Dictionary<string, string>();
+
+    public void AddBook(string isbn, string title)
+    {
+        if (!books.ContainsKey(isbn))
+        {
+            books.Add(isbn, title);
+            Console.WriteLine($"{title} eklendi.");
+        }
+        else
+        {
+            Console.WriteLine("Bu ISBN numarasına sahip bir kitap zaten var.");
+        }
+    }
+
+    public void DisplayBooks()
+    {
+        Console.WriteLine("\nKütüphanedeki kitaplar:");
+        foreach (var book in books)
+        {
+            Console.WriteLine($"ISBN: {book.Key}, Kitap Adı: {book.Value}");
+        }
+    }
+
+    public void RemoveBook(string isbn)
+    {
+        if (books.Remove(isbn))
+        {
+            Console.WriteLine("Kitap başarıyla silindi.");
+        }
+        else
+        {
+            Console.WriteLine("Kitap bulunamadı.");
+        }
+    }
+}
---
+++ Dictionary/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        Library library = new Library();
+
+        library.AddBook("978-0131101630", "The C Programming Language");
+        library.AddBook("978-0132350884", "Clean Code");
+        library.AddBook("978-0201616224", "The Pragmatic Programmer");
+
+        library.DisplayBooks();
+
+        library.RemoveBook("978-0131101630");
+
+        library.DisplayBooks();
+
+        Console.ReadKey();
+    }
+}
